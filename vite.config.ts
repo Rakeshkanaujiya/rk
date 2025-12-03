0a2306ee7f806e41ea82dd90b74b3066d9b8aa25
Rakesh Kanaujiya <https://github.com/rakeshkanaujiya>
2025-12-03 11:34:40 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), viteStaticCopy({
+      targets: [
+        {
+          src: 'public/404.html', // file you want to copy to dist
+          dest: '.' // root of dist folder
+        }
+      ]
+    }), mode === "development" && componentTagger()].filter(Boolean),
   base:'/rk/',
   resolve: {
     alias: {
